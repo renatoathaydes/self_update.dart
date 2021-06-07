e9212d1f70d70ce11d1a647e05a01b3a66e8006f
Renato Athaydes <renato@athaydes.com>
2021-06-07 22:18:11 +0200

    Fixed check of script name.

--- bin/dart_tests.dart
+++ bin/dart_tests.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+
+import 'package:path/path.dart' as p;
 import 'package:self_compile/dart_tests.dart';
 
 void main(List<String> arguments) async {
-  if (Platform.script.path == myexecFile.path &&
+  if (p.basename(Platform.script.path) == p.basename(myexecFile.path) &&
       arguments.contains('self-compile')) {
     await compileItself();
     return await exec(myexecFile.path);
